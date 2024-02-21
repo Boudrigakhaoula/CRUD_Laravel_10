1810f0b1abcd8b2741db60a3828ac2ce9991838a
Boudrigakhaoula <khaoulaboudriga@gmail.com>
2024-02-21 22:20:08 +0100

    crud laravel

---
+++ database/migrations/2024_02_21_162717_create_etudiant_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEtudiantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('classe');
+            $table->timestamps(); // Cette ligne ajoute automatiquement les horodatages updated_at et created_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('etudiants');
+    }
+}
---
+++ resources/views/etudiant/update.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRUD IN LARAVEL 10</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+
+<table>
+
+</table>
+
+<div class="container ">
+    <div class="row ">
+        <div class="col s12">
+            <h1>MODIFIER UN ETUDIANT - LARAVEL 10</h1>
+            <hr>
+            @if(session('status'))
+                <div class="alert alert-success">
+                    {{session('status')}}
+                </div>
+
+            @endif
+            <ul>
+                @foreach($errors->all() as $errors)
+                    <li class="alert alert-danger">
+                        {{$errors}}
+                    </li>
+                @endforeach
+            </ul>
+            <form action="/update/traitement" method="post">
+                @csrf
+
+
+                <input type="text" name="id" style="display: none;" value="{{$etudiants->id}}">
+                <div class="from-group">
+                    <label for="nom" class="form-label">Nom</label>
+                    <input type="text" class="form-control" id="nom" name="nom" value="{{$etudiants->nom}}" >
+                </div>
+                <div class="from-group">
+                    <label for="prenom" class="form-label">Prénom</label>
+                    <input type="text" class="form-control" id="prenom" name="prenom" value="{{$etudiants->prenom}}">
+                </div>
+                <div class="from-group">
+                    <label for="classe" class="form-label">Classe</label>
+                    <input type="text" class="form-control" id="classe" name="classe" value="{{$etudiants->classe}}">
+                </div>
+                <br>
+                <button type="submit" class="btn btn-primary">MODIFIER UN ETUDIANT</button>
+                <br> <br>
+                <a href="/etudiant" class="btn btn-danger">Revenir à liste des étudiants</a>
+            </form>
+
+
+        </div>
+
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
